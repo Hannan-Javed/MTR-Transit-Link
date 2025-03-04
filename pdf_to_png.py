67c37f9ef1dbdabe0e2b940475977f3458a87e76
Hannan-Javed <hannansahib@gmail.com>
2025-03-04 22:37:46 +0800

    revert commit

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -3,11 +3,11 @@ import os
 
 
 def convertToPNG():
+
     if not os.path.exists(os.getcwd() + "\\stations_png"):
         os.makedirs(os.getcwd() + "\\stations_png")
     for filename in os.listdir(os.getcwd()+"\\stations"):
         print("Converting ",filename,"to png....")
-        pdf = PdfDocument.FromFile(os.getcwd()+"\\stations\\"+filename)
+        pdf = ironpdf.PdfDocument.FromFile(os.getcwd()+"\\stations\\"+filename)
         pdf.RasterizeToImageFiles("stations_png/"+filename[:-4]+'.png',DPI=250)
-
-    print("Conversion complete")
+        
