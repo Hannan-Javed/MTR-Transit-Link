1114adf35f3e38321beec73d503b080b4b5d9553
Hannan-Javed <hannansahib@gmail.com>
2025-03-09 15:43:03 +0800

    handle cases of error when converting files to png

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -6,7 +6,18 @@ def convertToPNG():
 
     if not os.path.exists(os.getcwd() + "\\stations_png"):
         os.makedirs(os.getcwd() + "\\stations_png")
+    error_files = []
     for filename in os.listdir(os.getcwd()+"\\stations"):
         print("Converting ",filename,"to png....")
-        pdf = PdfDocument.FromFile(os.getcwd()+"\\stations\\"+filename)
-        pdf.RasterizeToImageFiles("stations_png/"+filename[:-4]+'.png',DPI=1000)
+        try:
+            pdf = PdfDocument.FromFile(os.getcwd()+"\\stations\\"+filename)
+            pdf.RasterizeToImageFiles("stations_png/"+filename[:-4]+'.png',DPI=1000)
+        except:
+            error_files.append(filename)
+    
+    if error_files:
+        print("Error converting the following files:")
+        for file in error_files:
+            print(file)
+
+convertToPNG()
