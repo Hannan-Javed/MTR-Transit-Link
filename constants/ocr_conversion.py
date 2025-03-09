45885a67d211720cd7cdcd97988cb75bda5e5899
Hannan-Javed <hannansahib@gmail.com>
2025-03-09 15:45:26 +0800

    refactor code

---
+++ constants/ocr_conversion.py
@@ -0,0 +1,31 @@
+LINE_CONVERSION = {
+    "Kwan O Line": "Tseung Kwan O Line",
+}
+
+STATION_CONVERSION = {
+    "Kennedy": "Kennedy Town",
+    "South": "South Horizons",
+    "Tsuen": "Tsuen Wan",
+    "Lo": "Lo Wu",
+    "Chai": "Chai Wan",
+    "Asia": "AsiaWorld-Expo",
+    "Tuen": "Tuen Mun",
+    "Wu": "Wu Kai Sha",
+    "Tiu": "Tiu Keng Leng",
+    "Sunny": "Sunny Bay",
+    "Po": "Po Lam",
+    "North": "North Point",
+    "Tung": "Tung Chung",
+    "機場 博覽 館": "AsiaWorld-Expo",
+    "or": "AsiaWorld-Expo",
+    "Hung": "Hung Hom",
+    "Disneyland": "Disneyland Resort",
+}
+
+END_CONVERSION = {
+    "Admiralty": "East Rail Line",
+    "Kennedy Town": "Island Line",
+    "North Point": "Island Line",
+    "Tiu Keng Leng": "Tseung Kwan O Line",
+    "Tsuen Wan": "Tsuen Wan Line",
+}
--- utils.py
+++ utils.py
@@ -1,35 +1,8 @@
 import os, re
 import json
-LINE_CONVERSION = {
-    "Kwan O Line": "Tseung Kwan O Line",
-}
-STATION_CONVERSION = {
-    "Kennedy": "Kennedy Town",
-    "South": "South Horizons",
-    "Tsuen": "Tsuen Wan",
-    "Lo": "Lo Wu",
-    "Chai": "Chai Wan",
-    "Asia": "AsiaWorld-Expo",
-    "Tuen": "Tuen Mun",
-    "Wu": "Wu Kai Sha",
-    "Tiu": "Tiu Keng Leng",
-    "Sunny": "Sunny Bay",
-    "Po": "Po Lam",
-    "North": "North Point",
-    "Tung": "Tung Chung",
-    "機場 博覽 館": "AsiaWorld-Expo",
-    "or": "AsiaWorld-Expo",
-    "Hung": "Hung Hom",
-    "Disneyland": "Disneyland Resort",
-}
-END_CONVERSION = {
-    "Admiralty": "East Rail Line",
-    "Kennedy Town": "Island Line",
-    "North Point": "Island Line",
-    "Tiu Keng Leng": "Tseung Kwan O Line",
-    "Tsuen Wan": "Tsuen Wan Line",
-}
-def clean_data():
+from constants.ocr_conversion import LINE_CONVERSION, STATION_CONVERSION, END_CONVERSION
+
+def clean_station_lines_data():
     os.makedirs('station_data_json', exist_ok=True)
     for file in os.listdir('stationdata'):
         with open('stationdata/'+file, 'r', encoding='utf-8') as f:
