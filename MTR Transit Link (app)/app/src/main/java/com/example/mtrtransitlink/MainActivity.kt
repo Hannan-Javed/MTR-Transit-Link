5c1392e73ccbd130841eeb4c747955f02e480d21
Hannan-Javed <hannansahib@gmail.com>
2025-03-21 15:31:57 +0800

    remove night mode

--- MTR Transit Link (app)/app/src/main/java/com/example/mtrtransitlink/MainActivity.kt
+++ MTR Transit Link (app)/app/src/main/java/com/example/mtrtransitlink/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mtrtransitlink
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.json.JSONObject
@@ -13,6 +14,7 @@ import java.util.Arrays
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
